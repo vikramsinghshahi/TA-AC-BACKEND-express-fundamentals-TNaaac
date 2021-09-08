cc3772f7a4d0659e7bec14a821147ebf1a1d5cce
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-09-08 10:54:43 +0530

    update

--- block-BNaabx/sample/server.js
+++ block-BNaabx/sample/server.js
@@ -2,6 +2,23 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.json());
+
+var logger = function (req, res, next) {
+  console.log(req.method, req.url);
+  next();
+};
+
+app.use(logger);
+
+app.get('/', (req, res) => {
+  res.send('welcome');
+});
+
+app.post('/json', (req, res) => {
+  res.send(req.body);
+});
+
 app.listen(3000, () => {
   console.log('server is listening at 3K');
 });
